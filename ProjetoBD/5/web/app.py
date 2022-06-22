2122dcdcab6e66555a44fed8f6fed511c2ab6127
Pacten15 <96833703+Pacten15@users.noreply.github.com>
2022-06-22 02:05:16 +0100

    Add files via upload

--- ProjetoBD/5/web/app.py
+++ ProjetoBD/5/web/app.py
@@ -19,9 +19,8 @@ DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def list_categorias():
+def list_categorias1():
     dbConn = None
     cursor = None
     try:
@@ -37,42 +36,67 @@ def list_categorias():
         dbConn.close()
 
 
-@app.route("/accounts")
-def list_accounts_edit():
+#a)
+#Inserir e Remover categorias
+@app.route("/categorias")
+def list_categorias2():
     dbConn = None
     cursor = None
     try:
         dbConn = psycopg2.connect(DB_CONNECTION_STRING)
         cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        query = "SELECT account_number, branch_name, balance FROM account;"
+        query = "SELECT * FROM categoria;"
         cursor.execute(query)
-        return render_template("accounts.html", cursor=cursor)
+        return render_template("categorias.html", cursor=cursor)
     except Exception as e:
-        return str(e)
+        return str(e)  # Renders a page with the error.
     finally:
         cursor.close()
         dbConn.close()
 
 
-@app.route("/balance")
-def change_balance():
+@app.route("/categorias-insert-form")
+def categorias_insert_form():
     try:
-        return render_template("balance.html", params=request.args)
+        return render_template("categorias-insert-form.html", params=request.args)
     except Exception as e:
         return str(e)
 
-
-@app.route("/update", methods=["POST"])
-def update_balance():
+@app.route("/categorias-insert-apply", methods=["POST"])
+def categorias_insert_apply():
     dbConn = None
     cursor = None
     try:
         dbConn = psycopg2.connect(DB_CONNECTION_STRING)
         cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        balance = request.form["balance"]
-        account_number = request.form["account_number"]
-        query = "UPDATE account SET balance=%s WHERE account_number = %s"
-        data = (balance, account_number)
+        new_name = request.form["new_name"]
+        query = "INSERT INTO categoria VALUES('{s}');".format(s=new_name)
+        data = (new_name)
+        cursor.execute(query, data)
+        return query
+    except Exception as e:
+        return str(e)
+    finally:
+        dbConn.commit()
+        cursor.close()
+        dbConn.close()
+
+@app.route("/categorias-remove")
+def categorias__remove():
+    dbConn = None
+    cursor = None
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        param = request.args
+        nome = param.get('nome')
+        query = "DELETE FROM categoria WHERE nome = '{s}'; \
+                 DELETE FROM produto WHERE cat = '{s}';\
+                 DELETE FROM tem_outra WHERE categoria='{s}';\
+                 DELETE FROM tem_outra WHERE super_categoria='{s}';\
+                 DELETE FROM prateleira WHERE nome='{s}';\
+                 DELETE FROM responsavel_por WHERE nome_cat ='{s}';".format(s=nome)
+        data = (nome)
         cursor.execute(query, data)
         return query
     except Exception as e:
@@ -83,5 +107,82 @@ def update_balance():
         dbConn.close()
 
 
+# (b)
+# Inserir e remover um retalhista, com todas suas as responsabilidades de reposições de produtos, 
+# garantindo que esta operação seja atómica
+
+@app.route("/retalhistas")
+def list_retalhistas():
+    dbConn = None
+    cursor = None
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        query = "SELECT * FROM retalhista;"
+        cursor.execute(query)
+        return render_template("retalhistas.html", cursor=cursor)
+    except Exception as e:
+        return str(e)
+    finally:
+        cursor.close()
+        dbConn.close()
+
+@app.route("/retalhistas-insert-form")
+def retalhistas_insert_form():
+    try:
+        return render_template("retalhistas-insert-form.html")
+    except Exception as e:
+        return str(e)
+
+@app.route("/retalhistas-insert-apply", methods=["POST"])
+def retalhistas_insert_apply():
+    dbConn = None
+    cursor = None
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        new_tin = request.form["new_tid"]
+        new_name = request.form["new_name"]
+        query = 'INSERT INTO retalhista VALUES(%s, %s);'
+        data = (new_tin, new_name)
+        cursor.execute(query, data)
+        return query
+    except Exception as e:
+        return str(e)
+    finally:
+        dbConn.commit()
+        cursor.close()
+        dbConn.close()
+
+@app.route("/retalhistas-remove")
+def retalhistas_remove():
+    dbConn = None
+    cursor = None
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        params = request.args
+        tin1 = tin2 = tin3 = params.get('tin')
+        query = 'DELETE FROM evento_reposicao WHERE tin = %s; \
+                 DELETE FROM responsavel_por WHERE tin = %s; \
+                 DELETE FROM retalhista WHERE tin = %s;'
+        data = (tin1, tin2, tin3)
+        cursor.execute(query, data)
+        return query
+    except Exception as e:
+        return str(e)
+    finally:
+        dbConn.commit()
+        cursor.close()
+        dbConn.close()
+
+#c
+#Listar todos os eventos de reposição de uma IVM, apresentando o número de unidades repostas por categoria de produto 
+
+
+
 CGIHandler().run(app)
+
+
+
 
