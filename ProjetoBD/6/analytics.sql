8ccdfe59dd3a102f154dacf76d2517766f463958
Franciscomaugusto <72284885+Franciscomaugusto@users.noreply.github.com>
2022-06-23 19:08:15 +0100

    Update analytics.sql

--- ProjetoBD/6/analytics.sql
+++ ProjetoBD/6/analytics.sql
@@ -4,3 +4,9 @@ WHERE dia_mes >= 2000-01-01
 AND dia_mes <= 2000-03-03
 GROUP BY
 ROLLUP (dia_semana, concelho, unidades);
+
+SELECT concelho, cat, dia_semana, unidades
+FROM vendas
+WHERE distrito = 'Lisboa'
+GROUP BY
+ROLLUP (concelho,cat, dia_semana, unidades);
