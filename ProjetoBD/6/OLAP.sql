95ad33e88bbaca56fe8557ed3bb188ce08af7839
Franciscomaugusto <72284885+Franciscomaugusto@users.noreply.github.com>
2022-06-24 17:48:20 +0100

    Update OLAP.sql

--- ProjetoBD/6/OLAP.sql
+++ ProjetoBD/6/OLAP.sql
@@ -1,6 +1,10 @@
 SELECT dia_semana, concelho, unidades
 FROM vendas
-WHERE dia_mes >= 2000-01-01
-AND dia_mes <= 2000-03-03
+WHERE ano >= EXTRACT(YEAR FROM '2000-01-01')
+AND ano <= EXTRACT(YEAR FROM '2000-03-03')
+AND mes >= EXTRACT(YEAR FROM '2000-01-01')
+AND mes <= EXTRACT(YEAR FROM '2000-03-03')
+AND dia_mes >= EXTRACT(YEAR FROM '2000-01-01')
+AND dia_mes <= EXTRACT(YEAR FROM '2000-03-03')
 GROUP BY
 ROLLUP (dia_semana, concelho, unidades);
